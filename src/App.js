11d1431800bd46804a2621ae0593b556f16c9e56
Ilo Tech <ilo.nikatsadze@gmail.com>
2024-03-08 22:48:50 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import box from './box'
 
 function App() {
+  const [massages,setMassages]=React.useState(box);
+  const [text,setText]=React.useState("Select all items")
+  function handleCheck(key){
+    setMassages(massages.map(it=>{
+      if(it.key===key){
+        it.checked=!it.checked;
+      }
+      return it
+      
+    }))
+  }
+  function handleDelete(){
+    setMassages(prewmassages=>prewmassages.filter(item=>!item.checked))
+  }
+
+  function chooseAll(){
+    setMassages(massages.map(item=>{
+      if(!item.checked){
+        setText("Unselect all items")
+      }
+      else{
+        setText("Select all items")
+      }
+      return {...item,checked:!item.checked}
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="hi">
+        <button onClick={chooseAll}>{text}</button>
+        <div className="massageContainer">
+          {massages.map(item=>{
+            return(<div key={item.key} style={{display:"flex",alignItems:"center"}}>
+              <input onChange={()=>handleCheck(item.key)} type="checkbox" style={{width:"30px",height:"30px"}} checked={item.checked}/>
+              <div>
+              {item.massage}
+              </div>
+              </div>)
+          })}
+        
+        </div>
+        <button classname="delete" onClick={handleDelete}>Delete</button>
+      </div>
+
   );
 }
 
---
+++ src/box.js
@@ -0,0 +1,8 @@
+const box=[
+    {name:"content 1",massage:"first massage",key:1,checked:false},
+    {name:"content 2",massage:"second massage",key:2,checked:false},
+    {name:"content 3",massage:"third massage",key:3,checked:false},
+    {name:"content 3",massage:"fourth massage",key:3,checked:false}
+ ]
+ export default box
+ 
